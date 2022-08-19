afaf706d017220d6a23c470d2f4611cab74975c9
GAMING\Usuario <pedro.moreno.trujillo@gmail.com>
2022-08-19 13:29:38 +0200

    created chips and check filtering

--- api/src/app/components/tabledata/tabledata.component.ts
+++ api/src/app/components/tabledata/tabledata.component.ts
@@ -5,6 +5,8 @@ import { ObservableHeroService } from 'src/app/services/observableHeroService';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
+import { MatChipInputEvent } from '@angular/material/chips';
+import { COMMA, ENTER } from '@angular/cdk/keycodes';
 
 @Component({
   selector: 'app-tabledata',
@@ -12,22 +14,65 @@ import { MatSort } from '@angular/material/sort';
   styleUrls: ['./tabledata.component.scss'],
 })
 export class TabledataComponent implements OnInit {
-  tableData!: MatTableDataSource<any>;
+  tableData: Hero[] = [];
   displayedHeader: string[] = Object.values(TableDataHeader);
   displayedColumns: string[] = Object.keys(TableDataHeader);
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
+  readonly separatorKeysCodes = [ENTER, COMMA] as const;
+  selectedFilters: string[] = [];
+  addOnBlur = true;
+  tableDataFiltered!: MatTableDataSource<any>;
 
-  constructor(private observableHeroService: ObservableHeroService) {
-  }
+  constructor(private observableHeroService: ObservableHeroService) {}
 
   ngOnInit(): void {
-    this.observableHeroService.heroList.subscribe(
-      (heroList) => {
-        this.tableData = new MatTableDataSource<Hero>(heroList)
-        this.tableData.paginator = this.paginator;
-        this.tableData.sort = this.sort;
-      })
+    this.observableHeroService.heroList.subscribe((heroList) => {
+      this.tableData = heroList;
+      this.tableDataFiltered = new MatTableDataSource<Hero>(heroList);
+      this.tableDataFiltered.paginator = this.paginator;
+      this.tableDataFiltered.sort = this.sort;
+    });
   }
 
+  add(event: MatChipInputEvent): void {
+    if (event.value.trim() !== '') {
+      this.selectedFilters.push(event.value.trim().toLocaleLowerCase());
+      event.chipInput!.clear();
+      this.filterDataForTable();
+    } else {
+      return;
+    }
+  }
+
+  remove(filter: any): void {
+    const index = this.selectedFilters.indexOf(filter);
+    if (index >= 0) {
+      this.selectedFilters.splice(index, 1);
+    }
+    if(this.selectedFilters.length === 0){
+      this.tableDataFiltered.data = this.tableData
+    }else{
+      this.filterDataForTable();
+    }
+
+  }
+
+  filterDataForTable() {
+    let filteredData: any[] = [];
+    let avoidDuplicatedHeroes: string[] = [];
+    this.tableData.forEach((hero: Hero) => {
+      this.selectedFilters.forEach((item) => {
+        if (hero.nameLabel.trim().toLowerCase().includes(item) && !avoidDuplicatedHeroes.includes(hero.nameLabel.trim().toLowerCase())) {
+          avoidDuplicatedHeroes.push(hero.nameLabel.trim().toLowerCase())
+          filteredData.push(hero);
+        }
+      });
+    });
+    this.tableDataFiltered.data = filteredData;
+  }
+
+  selectHero(hero: Hero){
+    console.log(hero)
+  }
 }
--- api/src/app/components/tabledata/tabledata.module.ts
+++ api/src/app/components/tabledata/tabledata.module.ts
@@ -4,10 +4,21 @@ import { TabledataComponent } from './tabledata.component';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [TabledataComponent],
   exports: [TabledataComponent],
-  imports: [CommonModule, MatTableModule, MatPaginatorModule, MatSortModule],
+  imports: [
+    CommonModule,
+    MatIconModule,
+    MatInputModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatChipsModule,
+  ],
 })
 export class TabledataModule {}
